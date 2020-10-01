197f17f8ec0b46659de6bfde837caf753be24e1a
James McManus <jmpmcmanus@adcirc-db.edc.renci.org>
2020-10-01 10:06:33 -0400

    Added swan63 query capability

--- region3-function-bufopt.sql
+++ region3-function-bufopt.sql
@@ -1,6 +1,6 @@
 CREATE OR REPLACE
--- http://localhost:7800/public.region3_sim_storms/13/2362/3138.pbf?&stormtable=var_dp3r3b1c1h1l1_fort63&timestep=2000-09-05T19:30:00&properties=node,zeta,bathymetry
-FUNCTION public.region3_sim_storms(z integer, x integer, y integer, stormtable text, timestep text)
+-- http://localhost:7800/public.region3_sim_storms_bufopt/13/2362/3138.pbf?buffer=0&stormtable=var_dp3r3b1c1h1l1_fort63&timestep=2000-09-05T19:30:00&properties=node,zeta,bathymetry
+FUNCTION public.region3_sim_storms_bufopt(z integer, x integer, y integer, buffer integer, stormtable text, timestep text)
 RETURNS bytea
 AS $$
 DECLARE
@@ -13,7 +13,7 @@ BEGIN
     EXECUTE format(
        'WITH
         bounds AS (
-          SELECT ST_TileEnvelope(%s, %s, %s) AS geomclip, ST_Expand(ST_TileEnvelope(%s, %s, %s), 256) AS geombuf 
+          SELECT ST_TileEnvelope(%s, %s, %s) AS geomclip, ST_Expand(ST_TileEnvelope(%s, %s, %s), %s) AS geombuf 
         ),
         node_ids AS
           (SELECT G.node, G.geom
@@ -27,13 +27,13 @@ BEGIN
              WHERE timestamp = %s 
              AND node IN (SELECT node FROM node_ids)) S
             LEFT JOIN
-            (SELECT ST_AsMVTGeom(ST_Transform(G.geom, 3857), bounds.geomclip, 4096, 256, true) AS geom, G.node AS node, G.bathymetry AS bathymetry
+            (SELECT ST_AsMVTGeom(ST_Transform(G.geom, 3857), bounds.geomclip, 4096, %s, true) AS geom, G.node AS node, G.bathymetry AS bathymetry
              FROM r3sim_fort_geom AS G, bounds
              WHERE ST_Intersects(G.geom, ST_Transform(bounds.geombuf, 4326))) T
             ON S.node = T.node
         )
         SELECT ST_AsMVT(mvtgeom, %s)
-        FROM mvtgeom;', z, x, y, z, x, y, quote_ident(stormtable), quote_literal(timestep), quote_literal('public.region3_sim_storms')
+        FROM mvtgeom;', z, x, y, z, x, y, buffer, quote_ident(stormtable), quote_literal(timestep), buffer, quote_literal('public.region3_sim_storms_bufopt')
     )
     INTO result;
     RETURN result;
@@ -41,7 +41,7 @@ BEGIN
     EXECUTE format(
        'WITH
         bounds AS (
-          SELECT ST_TileEnvelope(%s, %s, %s) AS geomclip, ST_Expand(ST_TileEnvelope(%s, %s, %s), 256) AS geombuf 
+          SELECT ST_TileEnvelope(%s, %s, %s) AS geomclip, ST_Expand(ST_TileEnvelope(%s, %s, %s), %s) AS geombuf 
         ),
         node_ids AS
           (SELECT G.node, G.geom
@@ -55,13 +55,13 @@ BEGIN
              WHERE timestamp = %s 
              AND node IN (SELECT node FROM node_ids)) S
             LEFT JOIN
-            (SELECT ST_AsMVTGeom(ST_Transform(G.geom, 3857), bounds.geomclip, 4096, 256, true) AS geom, G.node AS node, G.bathymetry AS bathymetry
+            (SELECT ST_AsMVTGeom(ST_Transform(G.geom, 3857), bounds.geomclip, 4096, %s, true) AS geom, G.node AS node, G.bathymetry AS bathymetry
              FROM r3sim_fort_geom AS G, bounds
              WHERE ST_Intersects(G.geom, ST_Transform(bounds.geombuf, 4326))) T
             ON S.node = T.node
         )
         SELECT ST_AsMVT(mvtgeom, %s)
-        FROM mvtgeom;', z, x, y, z, x, y, quote_ident(stormtable), quote_literal(timestep), quote_literal('public.region3_sim_storms')
+        FROM mvtgeom;', z, x, y, z, x, y, buffer, quote_ident(stormtable), quote_literal(timestep), buffer, quote_literal('public.region3_sim_storms_bufopt')
     )
     INTO result;
     RETURN result;
@@ -72,4 +72,4 @@ LANGUAGE 'plpgsql'
 VOLATILE
 PARALLEL SAFE;
 
-COMMENT ON FUNCTION public.region3_sim_storms IS 'Given a tile address, storm name, and timestamp query database.';
+COMMENT ON FUNCTION public.region3_sim_storms_bufopt IS 'Given a tile address, buffer, storm name, and timestamp query database.';
